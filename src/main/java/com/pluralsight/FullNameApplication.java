81e0da72914b86bff3d4ffabee2d6288173e5435
Craig McKeachie <craigdmckeachie@gmail.com>
2025-09-30 15:00:16 -0400

    Full Name Application v1

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Middle Name: ");
+        String middleName = scanner.nextLine();
+
+        System.out.println("Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Suffix: ");
+        String suffix = scanner.nextLine();
+
+        String fullName = firstName;
+
+        if (!middleName.isEmpty()) {
+            fullName = fullName + " " + middleName;
+        }
+
+        fullName = fullName + " " + lastName;
+
+        if (!suffix.isEmpty()) {
+            fullName = fullName + ", " + suffix;
+        }
+
+        System.out.println(fullName);
+
+
+    }
 }
